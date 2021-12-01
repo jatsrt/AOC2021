0d8ff69be250a5aa7dc6a1b376410fca4a7b192b
Jake Thompson <jake@zenlist.com>
2021-12-01 14:15:48 +0000

    Two different ways each one

--- lib/aoc2021.day01.ex
+++ lib/aoc2021.day01.ex
@@ -4,17 +4,31 @@ defmodule AOC2021.DAY01 do
   def run() do
     Logger.info("AOC 2021 Day 1")
 
-    case File.read("lib/aoc2021.day01.input") do
-      {:ok, result} ->
-        Logger.info("Input file read")
-        {:ok, input} = tokenize_file(result)
-        count_part_one = find_answer(input, 0)
-        Logger.info("The answer is part one: #{count_part_one}")
+    case open_file_contents("lib/aoc2021.day01.input") do
+      {:ok, contents} ->
+        simple_answer_part_one =
+          contents |> Enum.chunk_every(2, 1, :discard) |> Enum.map(&count_it/1) |> Enum.sum()
 
-        count_part_two =
-          find_answer(input |> Enum.chunk_every(3, 1, :discard) |> Enum.map(&Enum.sum/1), 0)
+        Logger.info("The simple answer is part one: #{simple_answer_part_one}")
 
-        Logger.info("The answer is part two: #{count_part_two}")
+        non_simple_answer_part_one = find_answer(contents, 0)
+
+        Logger.info("The non simple answer is part one: #{non_simple_answer_part_one}")
+
+        part_two_contents = contents |> Enum.chunk_every(3, 1, :discard) |> Enum.map(&Enum.sum/1)
+
+        simple_answer_part_two =
+          part_two_contents
+          |> Enum.chunk_every(2, 1, :discard)
+          |> Enum.map(&count_it/1)
+          |> Enum.sum()
+
+        Logger.info("The simple answer is part two: #{simple_answer_part_two}")
+
+        non_simple_answer_part_two = find_answer(part_two_contents, 0)
+
+        Logger.info("The non simple answer is part two: #{non_simple_answer_part_two}")
+
         {:ok}
 
       {:error, error} ->
@@ -23,21 +37,24 @@ defmodule AOC2021.DAY01 do
     end
   end
 
-  def tokenize_file(input) do
-    contents = input |> String.split("\n", trim: true) |> Enum.map(&String.to_integer/1)
-    Logger.info("Input Count: #{Enum.count(contents)}")
-    {:ok, contents}
+  def open_file_contents(path) do
+    Logger.info("Opening input file")
+
+    case File.read(path) do
+      {:ok, input} ->
+        contents = input |> String.split("\n", trim: true) |> Enum.map(&String.to_integer/1)
+        Logger.info("Input Count: #{Enum.count(contents)}")
+        {:ok, contents}
+
+      {:error, error} ->
+        {:error, error}
+    end
   end
 
-  def find_answer([h | [n | _] = t], acc) when h < n do
-    find_answer(t, acc + 1)
-  end
+  def count_it([first, second]) when first < second, do: 1
+  def count_it([_, _]), do: 0
 
-  def find_answer([h | [n | _] = t], acc) when h >= n do
-    find_answer(t, acc)
-  end
-
-  def find_answer([_ | []], acc) do
-    acc
-  end
+  def find_answer([h | [n | _] = t], acc) when h < n, do: find_answer(t, acc + 1)
+  def find_answer([_ | [_ | _] = t], acc), do: find_answer(t, acc)
+  def find_answer([_ | []], acc), do: acc
 end
